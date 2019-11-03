4c902924f849fbf0aa65fed8447f1f295233dca5
Eddie Hunsinger <43016799+edyhsgr@users.noreply.github.com>
2019-11-03 01:33:19 -0800

    Update USSexRatiosByAge.R

--- USSexRatiosByAge.R
+++ USSexRatiosByAge.R
@@ -1,4 +1,4 @@
-#Reviewing sex ratios by age, 1970 to 2015
+#Reviewing US sex ratios by age, 1970 to 2015
 #Eddie Hunsinger, November 2019
 #This code has not been carefully reviewed
 #I aimed for resident US, but for the 1970s it is currently resident US plus overseas military
